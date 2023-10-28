23658deb69512f066394d5e5a6a44f8e1ded6403
unknown <johangarciats2015@gmail.com>
2023-10-27 22:02:13 -0500

    blueprint

--- src/main/resources/static/js/apimock.js
+++ src/main/resources/static/js/apimock.js
@@ -6,14 +6,17 @@ apimock=(function(){
 
 	mockdata["johnconnor"]=	[{author:"johnconnor","points":[{"x":150,"y":120},{"x":215,"y":115}],"name":"house"},
 	 {author:"johnconnor","points":[{"x":340,"y":240},{"x":15,"y":215}],"name":"gear"}];
-	mockdata["maryweyland"]=[{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house2"},
-	 {author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"}];
+
+
+	mockdata["maryweyland"]=[{author:"maryweyland","points":[{"x":150,"y":150},{"x":300,"y":75}],"name":"house2"},
+	{author:"maryweyland","points":[{"x":150,"y":0},{"x":150,"y":150}],"name":"life"},
+	 {author:"maryweyland","points":[{"x":0,"y":75},{"x":150,"y":75}],"name":"gear2"}];
+
 	mockdata["johan"]= [{author:"johan","points":[{"x":150,"y":120},{"x":215,"y":115}],"name":"house3"},
-	{author:"johan","points":[{"x":340,"y":240},{"x":15,"y":215}],"name":"gear3"}];
+	{author:"johan","points":[{"x":150,"y":75},{"x":300,"y":0}],"name":"gear3"}];
+
 	mockdata["sebastian"]= [{author:"sebastian","points":[{"x":130,"y":110},{"x":123,"y":125}],"name":"house4"},
 	{author:"sebastian","points":[{"x":320,"y":140},{"x":152,"y":210}],"name":"gear4"}];
-
-
 
 	return {
 		getBlueprintsByAuthor:function(authname,callback){
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,6 +3,7 @@ var app = (function(api){
     //list of objects that contains, blueprint name, number of points of blueprint
     var bpname;
     var author;
+    var bpnames = {};
     //public function that allows to change the name of the selected author
     function changeAuthor(){
         $("#author").text(author + "'s Blueprints");
@@ -10,7 +11,9 @@ var app = (function(api){
 
     function getBlueprintsAuthor(){
         author = $("#author-input").val();
+        
         api.getBlueprintsByAuthor(author,blueprints);
+
     }
 
     var blueprints = function(data){
@@ -28,7 +31,7 @@ var app = (function(api){
                 }
             });
             newdata.map((elements) => {
-                $("#table > tbody:last").append($("<tr><td>" + elements.name + "</td><td>" + elements.points.toString()+ "</td><td>" + "<button id=" + elements.name + " onclick=app.getBlueprintsAuthorAndname('"+ author + "','" + elements.name + "')>open</button>" + "</td>"));
+                $("#table > tbody:last").append($("<tr><td>" + elements.name + "</td><td>" + elements.points.toString()+ "</td><td>" + "<button id=" + elements.name + " onclick=app.getBlueprintsByNameAndAuthor('"+ author + "','" + elements.name + "')>open</button></td></tr>"));
             });
             const total = newdata.reduce((suma,{points}) => suma + points , 0);
 
@@ -36,28 +39,116 @@ var app = (function(api){
         }  
     }
 
-    function getBlueprintsAuthorAndname(author,bpname){
-        api.getBlueprintsAuthorAndname(author,bpname);
+    function getBlueprintsByNameAndAuthor(author,bpname){
+        api.getBlueprintsByNameAndAuthor(author,bpname,canvas);
+
     }
 
     var canvas = function(data){
-        $(document).ready(function(){
+        $(document).ready(function(){   
+            bpnames = data;
             var canvas = document.getElementById("myCanvas");
-            var contexto = c.getContext("2d");
-            canvas.width = canvas.witdth;
+            var contexto = canvas.getContext("2d");
+            canvas.width = canvas.width;
+            
+            contexto.lineWidth=3;
+            contexto.strokeStyle="black";
+            contexto.beginPath();
+            contexto.moveTo(0,0);
             data.points.map((element) => {
                 contexto.lineTo(element.x,element.y);
             });
-            contexto.fillStyle ="red";
+            contexto.fillStyle ="orange";
             contexto.fill();
-            contexto.strokeStyle="black";
-            contexto.lineWidth=2;
             contexto.stroke();
         });
     }
+
+    function init(){
+        $(document).ready(function() {
+            var canvas = document.getElementById("myCanvas");
+            var ctx = canvas.getContext("2d");
+                if(window.PointerEvent) {
+                    console.info('initialized');
+                    canvas.addEventListener("pointerdown", function(event){
+                    var offset = _getOffset(canvas);
+                    console.log(event.pageX - offset.left);
+                    bpnames.points.push({x:event.pageX - offset.left, y:event.pageY - offset.top});
+                    updatecanvas(bpnames);
+                    }
+                    );
+                }
+                else {
+                    console.info('initialized');
+                    canvas.addEventListener("mousedown", function(event){
+                    var offset = _getOffset(canvas);
+                    console.log(event.pageX - offset.left);
+                    bpnames.points.push({x:event.pageX - offset.left, y:event.pageY - offset.top});
+                    updatecanvas(bpnames);
+                }
+                );
+                }
+            });
+    }
+
+    let updatecanvas = (data) =>{
+        canvas(data);
+        bpnames = data;
+        
+    }
+
+
+    let _getOffset = function (obj) {
+        var offsetLeft = 0;
+        var offsetTop = 0;
+        do {
+            if (!isNaN(obj.offsetLeft)) {
+                offsetLeft += obj.offsetLeft;
+            }
+            if (!isNaN(obj.offsetTop)) {
+                offsetTop += obj.offsetTop;
+            }
+        } while(obj = obj.offsetParent );
+        return {left: offsetLeft, top: offsetTop};
+    }
+
+    function saveUpdate(){
+        api.putBlueprint(bpnames);
+        getBlueprintsAuthor();
+    }
+
+    function newBlueprint(){
+        clearCanva();
+        var name = prompt("Ingrese el Nuevo nombre del plano");
+        bpnames = {
+            author: author,
+            name: name,
+            points: []
+        };
+        getBlueprintsAuthor();
+    }
+
+    function clearCanva(){
+        var canvas = document.getElementById("myCanvas");
+        var ctx = canvas.getContext("2d");
+        canvas.width = canvas.width;
+    }
+
+    function deleteBlueprint(){
+        api.deleteBlueprint(bpnames);
+        clearCanva();
+        bpnames={};
+        getBlueprintsAuthor();
+    }
+
     return{
         changeAuthor:changeAuthor,
         getBlueprintsAuthor:getBlueprintsAuthor,
-        getBlueprintsAuthorAndname:getBlueprintsAuthorAndname
+        getBlueprintsByNameAndAuthor:getBlueprintsByNameAndAuthor,
+        init:init,
+        saveUpdate:saveUpdate,
+        newBlueprint:newBlueprint,
+        clearCanva:clearCanva,
+        deleteBlueprint:deleteBlueprint
     }
 })(apimock);
